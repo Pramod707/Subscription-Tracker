0c7924c0b51ae4461efeda2e0195f7d984342b9a
Pramod <pramod4342@gmail.com>
2025-07-24 22:13:55 +0530

    ..

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ import authRouter from "./ROUTES/auth.route.js";
 import userRouter from "./ROUTES/user.route.js";
 import subscriptionRouter from "./ROUTES/subscrition.route.js";
 import  DB_connection from "./database/mongo_db.js";
+import errorMiddleware from "./middleWare/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+app.use(errorMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser);
+
 
 app.get('/', (req,res)=>{
     res.send("welcome to subscription Tracker API");
--- middleWare/error.middleware.js
+++ middleWare/error.middleware.js
@@ -1,11 +1,38 @@
-    const errorMiddleware = (err,req,res,next)=>{
-        try {
-            
-            return res.status(500).json({
-                message : err.message
-            })
+const errorMiddleware = (err, req, res, next) => {
+  try {
+let error = {...err};
+error.message = err.message;
 
-        } catch (error) {
-              console.log(error);
-        }
-    }
+//mongoose bad object
+
+if(err.name === 'CastError'){
+    const message = `Resource not found. Invalid: ${err.path}`;
+    error = new Error(message);
+    error.statusCode = 400;
+}
+
+//mongoose duplicate key error
+if(err.code === 11000){
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    error = new Error(message);
+    error.statusCode = 400;
+}
+
+//mongoose validation error
+
+if(err.name === 'ValidationError'){
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new Error(message);
+    error.statusCode = 400;
+}
+
+    return res.status(error.statusCode || 500).json({
+      success: false,
+      error: error.message || "Server Error",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default errorMiddleware;
